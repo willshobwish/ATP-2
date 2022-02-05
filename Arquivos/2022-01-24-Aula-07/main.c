031a7eab2c81e416b6d02240d3793d0d76b60996
willshobwish <willianyos13@gmail.com>
2022-02-05 01:53:25 -0300

    Modificar a indentação e comentários do código

--- Arquivos/2022-01-24-Aula-07/main.c
+++ Arquivos/2022-01-24-Aula-07/main.c
@@ -1,14 +1,11 @@
+#include <stdbool.h>  // stdbool.h para variaveis booleanos
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-// time.h para funcao time()
-#include <stdbool.h>
-// stdbool.h para variaveis booleanos
+#include <time.h>  // time.h para funcao time()
 
-// numero maximo de numeros aleatorios
-#define NUMEROMAX 400000
+#define NUMEROMAX 400000  // numero maximo de numeros aleatorios
 
-int LeituraDimensaoArray(void) {
+int LeituraDimensaoArray(void) {  // funcao para definir o tamanho do array dentro de limitacoes
     int dimensao;
     bool continuar;
     do {
@@ -21,36 +18,33 @@ int LeituraDimensaoArray(void) {
         } else {
             continuar = false;
         }
-    } while (continuar);
+    } while (continuar);  // enquanto "continuar" possui o valor verdadeiro continuara repetindo, ate que entre com os valores dentro dos parametros
     return (dimensao);
 }
 
-void AssociaValoresArray(int *ponteiro, int dim) {
+void AssociaValoresArray(int *ponteiro, int dim) {  // funcao para associar os valores pseudoaleatorios no vetor gerado
     int i = 0;
-    srand((unsigned)time(NULL));
-    // geracao de numeros aleatorios para matriz
-    // srand
-    for (int i = 0; i < dim; i++) {
-        *(ponteiro + i) = rand();
+    srand((unsigned)time(NULL));     // geracao de numeros aleatorios para matriz
+    for (int i = 0; i < dim; i++) {  // cada vez ira percorrer o vetor e inserindo inteiros pseudoaleatorios
+        *(ponteiro + i) = rand();    //*(ponteiro + i) == ponteiro[i]
     }
     return;
 }
 
-void ImpressaoValoresArray(int *ponteiro, int dim) {
-    for (int i = 0; i < dim; i++) {
-        // for para percorrer array
-        printf("\n[%d] = %d", i, *(ponteiro + i));
+void ImpressaoValoresArray(int *ponteiro, int dim) {  // funcao para impressao dos valores contidos no vetor no console
+    for (int i = 0; i < dim; i++) {                   // for para percorrer o vetor
+        printf("\n[%d] = %d", i, *(ponteiro + i));    // cada vez que percorre, imprime no console o valor
     }
 }
 
-void troca(int *s, int indice1, int indice2) {
+void troca(int *s, int indice1, int indice2) {  // funcao para trocar os elementos dos vetores para ordenacao
     int auxiliar;
     auxiliar = *(s + indice1);
     *(s + indice1) = *(s + indice2);
     *(s + indice2) = auxiliar;
 }
 
-void SelectionSort(int *ponteiro, int dim) {
+void SelectionSort(int *ponteiro, int dim) {  // funcao para ordenacao do tipo "selection sort" sem indice
     int i, j, limite = dim - 1;
     for (i = 0; i < limite; i++) {
         for (j = i + 1; j < dim; j++) {
@@ -61,21 +55,9 @@ void SelectionSort(int *ponteiro, int dim) {
 }
 
 int main(void) {
-    int tamanhoArray;
-    // tamanho real do array vetor 1
-
-    int vetor1[NUMEROMAX];
-    // array vetor 1 para armazenar os elementos
-    // um vetor eh ponteiro
-
-    tamanhoArray = LeituraDimensaoArray();
-    // chamada de funcao com retorno
-    // a funcao verifica se esta correto ou nao
-
-    AssociaValoresArray(vetor1, tamanhoArray);
-    // void AssociaValoresArray(int *ponteiro, int dim)
-    // funcao para associar valores ao vetor
-
-    ImpressaoValoresArray(vetor1, tamanhoArray);
-    // void ImpressaoValoresArray(int *ponteiro, int dim)
+    int tamanhoArray;                             // tamanho real do array vetor 1
+    int vetor1[NUMEROMAX];                        // array vetor 1 para armazenar os elementos, um vetor eh ponteiro
+    tamanhoArray = LeituraDimensaoArray();        // chamada de funcao com retorno,  a funcao verifica se esta correto ou nao
+    AssociaValoresArray(vetor1, tamanhoArray);    // void AssociaValoresArray(int *ponteiro, int dim), funcao para associar valores ao vetor
+    ImpressaoValoresArray(vetor1, tamanhoArray);  // void ImpressaoValoresArray(int *ponteiro, int dim)
 }
