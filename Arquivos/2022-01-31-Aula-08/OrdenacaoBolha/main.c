dbcec96be590f16d7363680db8d360efafd9717a
willshobwish <willianyos13@gmail.com>
2022-02-05 11:54:54 -0300

    Criar arquivos do Bubble Sort

---
+++ Arquivos/2022-01-31-Aula-08/OrdenacaoBolha/main.c
@@ -0,0 +1,81 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAMANHOMAXIMO 40000
+
+int LeituraDimensaoVetor() {  // funcao que define o tamatnho do vetor a partir da entrada do usuario
+    int dimensao;
+    bool continua = true;
+    do {
+        printf("Insira a quantidade de componentes no array (>= 1 e <= %d): ", TAMANHOMAXIMO);
+        scanf("%d", &dimensao);
+        if ((dimensao <= 0) && (dimensao > TAMANHOMAXIMO)) {  // caso a condicao nao esteja dentro dos parametros ira repetir o processo
+            printf("0 < numero de componentes <= %d", TAMANHOMAXIMO);
+        } else {
+            continua = false;
+        }
+    } while (continua);
+    return dimensao;
+}
+
+void AssociarValoresVetor(int *ponteiro, int dimensao) {  // funcao que gera valores pseudoaleatorios para preencher o vetor com elementos numericos
+    time_t tempo;
+    srand((unsigned)time(&tempo));        // void srand(unsigned int seed), precisa de uma "semente" para gerar numeros pseudoaleatorios
+    for (int i = 0; i < dimensao; i++) {  // for para percorrer o vetor e atribuir valores aos elementos do vetor
+        *(ponteiro + i) = rand();         // atribuicao em cada elemento
+    }
+    return;
+}
+
+void ImprimirValoresVetor(int *ponteiro, int dimensao) {
+    for (int i = 0; i < dimensao; i++) {
+        printf("[%d] = %d\n", i, *(ponteiro + i));
+    }
+}
+
+// Exercicio
+// Elaborar uma funcao de troca com apenas dois parametros
+void Troca(int *posicao, int indice1, int indice2) {
+    int auxiliar;
+    auxiliar = *(posicao + indice1);
+    *(posicao + indice1) = *(posicao + indice2);
+    *(posicao + indice2) = auxiliar;
+}
+
+void OrdenacaoBolha(int *elemento, int dimensao) {
+    int i = 1, j, limite;
+    bool continuar = true;
+    while ((i < dimensao) && (continuar)) {
+        continuar = false;
+        limite = dimensao - i;
+        for (j = 0; j < limite; j++) {
+            if (*(elemento + j) > *(elemento + j + 1)) {
+                Troca(elemento, j, j + 1);
+                continuar = true;
+            }
+        }
+        i++;
+    }
+    return;
+}
+
+// Exercicio
+// Elaborar e testar a varredura da direita para esquerda
+
+int main() {
+    int tamanhoVetor, vetor[TAMANHOMAXIMO];
+    tamanhoVetor = LeituraDimensaoVetor();
+    AssociarValoresVetor(vetor, tamanhoVetor);
+    puts("Vetor original:");
+    ImprimirValoresVetor(vetor, tamanhoVetor);
+    OrdenacaoBolha(vetor, tamanhoVetor);
+    puts("\nVetor Ordenado:");
+    ImprimirValoresVetor(vetor, tamanhoVetor);
+    puts("");
+    printf("Pressione enter/return para finalizar...");
+    fflush(stdin);
+    getchar();
+    return 0;
+}
