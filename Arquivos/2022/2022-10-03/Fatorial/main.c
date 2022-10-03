a94c8be25b9f12dce7f3691227b8b67632c331af
Willian Murayama <willianyos13@gmail.com>
2022-10-03 16:16:54 -0300

    Modificação da formatação do código

--- Arquivos/2022/2022-10-03/Fatorial/main.c
+++ Arquivos/2022/2022-10-03/Fatorial/main.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int n,r;
+int main() {
+    int n, r;
     printf("Digite um numero, 0 <= n <= 19, para o calculo de seu Fatorial: ");
-    scanf("%i",&n);
-    getchar(); // limpando o buffer de entrada
+    scanf("%i", &n);
+    getchar();  // limpando o buffer de entrada
     if (n < 0 || n > 19) {
         printf("O numero deve ser 0 <= n <= 19\n");
-    }
-    else {
-        fatorialRecRef(n,&r);
-        printf("O Fatorial [recursivo, retorna em parametro] de %i eh %i\n",n,r);
+    } else {
+        fatorialRecRef(n, &r);
+        printf("O Fatorial [recursivo, retorna em parametro] de %i eh %i\n", n, r);
         r = fatorialRecVal(n);
-        printf("O Fatorial [recursivo, retorna valor] de %i eh %i\n",n,r);
+        printf("O Fatorial [recursivo, retorna valor] de %i eh %i\n", n, r);
         r = fatorialNRecVal(n);
-        printf("O Fatorial [nao recursivo, retorna valor] de %i eh %i\n",n,r);
+        printf("O Fatorial [nao recursivo, retorna valor] de %i eh %i\n", n, r);
     }
 
     printf("Pressione enter/return para finalizar ");
