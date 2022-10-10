bc1db385ace41d638668c6c43bae4c8531e1d763
Willian Murayama <willianyos13@gmail.com>
2022-10-10 15:29:06 -0300

    Implementação de funções

--- Arquivos/2022/2022-10-10/MatrizPonteiroPonteiro/main.c
+++ Arquivos/2022/2022-10-10/MatrizPonteiroPonteiro/main.c
@@ -2,12 +2,30 @@
 #include <stdlib.h>
 
 float **caloca(int lin, int col) {
+    int i;
+    float **pm;
+    pm = (float **)calloc(lin, sizeof(float *));
+    for (i = 0; i < lin; i++) {
+        pm[i] = (float *)calloc(col, sizeof(float));
+    }
+    return (pm);
 }
 
 void clibera(int lin, float **pm) {
+    int i;
+    for (i = 0; i < lin; i++) {
+        free(pm[i]);
+    }
+    return (pm);
 }
 
 void somaMatrizes(float **m1, float **m2, float **m3, int l, int c) {
+    int i, j;
+    for (i = 0; i < l; i++) {
+        for (j = 0; j < c; j++) {
+            m3[i][j] = m1[i][j] + m2[i][j];
+        }
+    }
 }
 
 int main() {
