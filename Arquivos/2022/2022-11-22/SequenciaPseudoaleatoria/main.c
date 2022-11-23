6f804505372637b6e0344368dcf90fffa75f6694
Willian Murayama <willianyos13@gmail.com>
2022-11-23 09:42:27 -0300

    Modificação do código
    Remoção da entrada de nome de arquivo texto
    Correção da primeira sequencia da equação
    Adição de caracteres na impressão do arquivo texto
    Remoção do arquivo texto "teste"

--- Arquivos/2022/2022-11-22/SequenciaPseudoaleatoria/main.c
+++ Arquivos/2022/2022-11-22/SequenciaPseudoaleatoria/main.c
@@ -3,37 +3,41 @@
 #include <string.h>
 #include <time.h>
 
-#define MAX 10
+#define MAX 20
 #define MAXFILENAMESIZE 50
 
 FILE *File;
 
 int main() {
-    char FileName[MAXFILENAMESIZE];
-    int FileNameSize, x0, xi, a, b, c, resultado;
-    printf("Coloque os numeros de x0,a,b e c: ");
+    int x0, xi, a, b, c, resultado;
+    printf("Coloque os numeros de x0,a,b e c:\n");
     scanf("%d %d %d %d", &x0, &a, &b, &c);
     printf("\nx0:%d\na:%d\nb:%d\nc:%d\n", x0, a, b, c);
 
-    printf("Coloque o nome do arquivo para gravar os numeros: ");
-    fflush(stdin);
-    fgets(FileName, MAXFILENAMESIZE, stdin);
-    FileNameSize = strlen(FileName) - 1;
-    FileName[FileNameSize] = '\0';
-    if ((File = fopen(FileName, "w")) == NULL) {
-        printf("Erro na abertura do arquivo %s", FileName);
+    // printf("Coloque o nome do arquivo para gravar os numeros: ");
+    // fflush(stdin);
+    // fgets(FileName, MAXFILENAMESIZE, stdin);
+    // FileNameSize = strlen(FileName) - 1;
+    // FileName[FileNameSize] = '\0';
+
+    if ((File = fopen("Resultado.txt", "w")) == NULL) {
+        printf("Erro na abertura do arquivo 'Resultado.txt'\n");
         exit(1);
     } else {
         printf("Arquivo aberto com sucesso\n");
     }
-    for (int xi = 0; xi < MAX; xi++) {
-        resultado = (a * x0 + b) % c;
+    for (int xi = 1; xi <= MAX; xi++) {
+        if (xi != 1) {
+            resultado = (a * x0 + b) % c;
+        } else {
+            resultado = x0;
+        }
         printf("Numero gerado na posicao %d: %d\n", xi, resultado);
-        fprintf(File, "%d\n", resultado);
+        fprintf(File, "Número gerado na posição %d: %d\n", xi, resultado);
         x0 = resultado;
     }
     if (fclose(File) != 0) {
-        printf("Erro no fechamento do arquivo: %s", File);
+        printf("Erro no fechamento do arquivo: %s\n", File);
     } else {
         printf("Arquivo fechado com sucesso\n");
     }
